97416674f64bcbbed3d8480358a8ccb428e1b07e
Pluck Yang <pluck.yang@gmail..com>
2024-08-08 15:55:02 +0900

    add game-board, add gitignore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Player from "./components/Player";
+import Player from "./components/Player.js";
+import GameBoard from "./components/GameBoard.js";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
---
+++ src/components/GameBoard.js
@@ -0,0 +1,40 @@
+import { useCallback, useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handelSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updateGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updateGameBoard[rowIndex][colIndex] = "X";
+      //console.log("click!!!");
+      return updateGameBoard;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handelSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
