2ec11d0ad687f899a5833826e6fd8a7eac1abbe8
Pluck Yang <pluck.yang@gmail..com>
2024-08-15 14:32:16 +0900

    reduce unnecessary state component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import GameBoard from "./components/GameBoard.js";
 import { useState } from "react";
 import Log from "./components/Log.js";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
 
+  const activePlayer = deriveActivePlayer(gameTurns);
+
   function handelSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((preTurns) => {
       //使用activePlayer变量虽然方便但会引入其他state，并不是最佳处理，state之间尽量不互相影响
-      let currentPlayer = "X";
-
-      if (preTurns.length > 0 && preTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = deriveActivePlayer(preTurns);
 
       const updatedTurns = [
         { suqare: { row: rowIndex, col: colIndex }, player: currentPlayer },
