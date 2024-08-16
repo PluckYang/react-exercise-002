ddcad29f9ee9f36708e73faa6a4ddc61fd37f385
Pluck Yang <pluck.yang@gmail..com>
2024-08-16 16:29:59 +0900

    add winner name showing logic

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState("X");
   //const [hasWinner, setHasWinner] = useState(false);
   const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState({ X: "Player 1", O: "Player 2" });
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
@@ -52,7 +53,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -77,6 +78,15 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -85,11 +95,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(hasDraw || winner) && (
