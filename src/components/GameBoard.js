dac17ddcfe39afb494672d8ed2e7073b0accebeb
Pluck Yang <pluck.yang@gmail..com>
2024-08-16 11:13:03 +0900

    add winner checking logic

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,36 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { suqare, player } = turn;
-    const { row, col } = suqare;
-
-    gameBoard[row][col] = player;
-  }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handelSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updateGameBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updateGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updateGameBoard;
-  //   });
-
-  //   onSelectSquare();
-  // }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
